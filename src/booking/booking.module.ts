040f731cb7b28ac8b18ebc1e6ebeaa83da63e311
Mohamed Osama <141563962+mohamed-osamaaa@users.noreply.github.com>
2025-07-23 12:13:20 +0300

    add email to booking function

--- src/booking/booking.module.ts
+++ src/booking/booking.module.ts
@@ -4,10 +4,12 @@ import { BookingResolver } from './booking.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Booking } from './entities/booking.entity';
 import { Service } from 'src/services/entities/service.entity';
+import { MailModule } from 'src/utility/nodemailer/mail.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Booking, Service]),
+    MailModule,
   ],
   providers: [BookingResolver, BookingService],
 })
